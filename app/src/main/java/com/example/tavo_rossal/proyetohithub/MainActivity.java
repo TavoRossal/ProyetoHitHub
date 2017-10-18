64ef067694758c45d5302f640708696e6e033eff
Tavo_Rossal <gustrago@gmail.com>
2017-10-17 18:24:46 -0600

    Es solo un mensaje que no le cuadra a esta mierda
    Se agregaron los strings

---
+++ app/src/main/java/com/example/tavo_rossal/proyetohithub/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tavo_rossal.proyetohithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
